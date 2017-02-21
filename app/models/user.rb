e98bdd414341ab392ebbd34267796b1bdf65f20e
Ikeda Takanao <ttttt.iiiii.tip@gmail.com>
2017-02-21 15:41:59 +0000

    add followings/followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,25 @@ class User < ActiveRecord::Base
     validates :email, presence: true, length: {maximum: 255},format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sesitive: false}
     has_secure_password
     has_many :microposts
+    has_many :following_relationships, class_name: "Relationship",
+                                        foreign_key: "follower_id",
+                                        dependent: :destroy
+    has_many :following_users, through: :following_relationships, source: :followed
+    
+    #他のユーザーをフォロー
+    def follow(other_user)
+       following_relationships.find_or_create_by(followed_id: other_user.id) 
+    end
+    
+    #フォローしているユーザーをアンフォローする
+    def unfollow(other_user)
+       following_relationship = following_relationship.find_by(followed_id: other_user.id) 
+       following_relationship.destroy if following_relationship
+    end
+    
+    #あるユーザーをフォローしているかどうか
+    def following?(other_user)
+        following_users.include?(other_user)
+    end
+    
 end
