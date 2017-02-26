f81af64727adad0dc619f9d869fd3275f40346cd
Ikeda Takanao <ttttt.iiiii.tip@gmail.com>
2017-02-26 14:34:50 +0000

    syuusei

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
     
     #フォローしているユーザーをアンフォローする
     def unfollow(other_user)
-       following_relationship = following_relationship.find_by(followed_id: other_user.id) 
+       following_relationship = following_relationships.find_by(followed_id: other_user.id) 
        following_relationship.destroy if following_relationship
     end
     
